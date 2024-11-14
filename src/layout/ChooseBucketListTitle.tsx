807ea69f3ea06ba8c69055e6eb03b8cd4ebdf81b
Pierre Jarry <pierre.jarry82@gmail.com>
2024-11-14 11:56:15 +0100

    Added possibility to validate the title by pressing enter

--- src/layout/ChooseBucketListTitle.tsx
+++ src/layout/ChooseBucketListTitle.tsx
@@ -2,15 +2,21 @@ import { useBucketList } from "../context/bucketListContext"
 import Button from "../components/Button";
 
 function ChooseBucketListTitle() {
-    const { 
+    const {
         title,
-        setTitle, 
-        setShowList 
+        setTitle,
+        setShowList
     } = useBucketList();
 
-  const showBucketList = () => {
-    setShowList(title === '' ? false : true);
-  }
+    const showBucketList = () => {
+        setShowList(title === '' ? false : true);
+    }
+
+    const showBucketListOnEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.keyCode !== 13) return;
+        e.preventDefault();
+        showBucketList();
+    }
 
     return (
         <>
@@ -22,15 +28,16 @@ function ChooseBucketListTitle() {
                     placeholder='Example: My Bucket List'
                     value={title}
                     onChange={e => setTitle(e.target.value)}
+                    onKeyDown={showBucketListOnEnter}
                 />
                 <div className='btn-container'>
-                    <Button 
+                    <Button
                         type='secondary'
                         title='Empty textfield'
                         text='Reset'
                         action={() => setTitle('')}
                     />
-                    <Button 
+                    <Button
                         type='primary'
                         title='Validate title'
                         text='Validate'
