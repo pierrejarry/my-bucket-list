a59234e97defe916be763d24aa6f7a4daaa98300
Pierre Jarry <pierre.jarry82@gmail.com>
2024-11-22 11:18:33 +0100

    Setup vitest and wrote a test for ChooseBucketListTitle component

---
+++ tests/components/ChooseBucketListTitle.test.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ChooseBucketListTitle from '../../src/layout/ChooseBucketListTitle';
+import { BucketListProvider } from '../../src/context/bucketListContext';
+import userEvent from '@testing-library/user-event';
+
+describe('ChooseBucketListTitle component', () => {
+    function setup() {
+        render(
+            <BucketListProvider>
+                <ChooseBucketListTitle />
+            </BucketListProvider>
+        );
+
+        const title = screen.getByRole('heading', { name: /My Bucket List/i });
+        const textFields = screen.getAllByRole('textbox');
+        const validateButton = screen.getByRole('button', { name: /validate/i });
+        const totalButtons = screen.getAllByRole('button');
+
+        return {
+            title,
+            textFields,
+            validateButton,
+            totalButtons,
+            user: userEvent.setup(), // Include userEvent to avoid multiple setups
+        };
+    }
+
+    it('it shows a title, 2 text fields and 2 buttons (one button is disabled)', () => {
+        const { 
+            title, 
+            textFields, 
+            validateButton, 
+            totalButtons 
+        } = setup();
+
+        /* Find title */
+        expect(title).toBeInTheDocument();
+
+        /* Find 2 text fields */
+        expect(textFields).toHaveLength(2);
+
+        /* Find 2 buttons */
+        expect(totalButtons).toHaveLength(2);
+
+        /* Validate button is disabled */
+        expect(validateButton).toBeDisabled();
+    });
+
+    it('it the user writes a title, it enables the "Validate" button', async () => {
+        const { user, validateButton } = setup();
+
+        /* Find textfield to write the title */
+        const titleField = screen.getByRole('textbox', { name: /choose a name/i });
+
+        /* Enable button when user type something */
+        await user.click(titleField);
+        await user.type(titleField, 'test');
+
+        expect(validateButton).toBeEnabled();
+    })
+})
