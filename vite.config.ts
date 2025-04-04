7ab5a9164fdb1d05f1efd25e7baa4aced582fb70
Pierre Jarry <pierre.jarry82@gmail.com>
2025-04-04 12:26:11 +0200

    Added options to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/my-bucket-list/',
-  /* build: {
-    assetsDir: 'assets'
-  } */
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+  }
 })
