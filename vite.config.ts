68efa09b41d9517f3430e81eccb663f5ec68df9b
Pierre Jarry <pierre.jarry82@gmail.com>
2025-04-04 12:46:06 +0200

    Added base again

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /* base: '/my-bucket-list/',
-  build: {
-    assetsDir: 'assets',
-    outDir: 'dist',
-  } */
+  base: '/my-bucket-list/'
 })
