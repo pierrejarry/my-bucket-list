2e5023bdbb9693e5c9e30cf527d951575fae8bb4
Pierre Jarry <pierre.jarry82@gmail.com>
2025-04-04 12:11:47 +0200

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/my-bucket-list/',
-  build: {
+  /* build: {
     assetsDir: 'assets'
-  }
+  } */
 })
